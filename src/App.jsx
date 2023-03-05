793da145d241da7fa88cfc7638e6fbb256748365
davidhorvath2020 <davidhorvath2020@gmail.com>
2023-03-05 17:25:41 +0100

    firtst Commit myself

--- src/App.jsx
+++ src/App.jsx
@@ -3,37 +3,47 @@ import { useEffect } from 'react';
 
 
 function App() {
-
+  const item = {}
   const [darkMode, setDarkMode] = useState(false);
 
   const [newItem, setNewItem] = useState("");
+  const [newTitle, setnewTitle] = useState("");
   const [items, setItems] = useState(
     JSON.parse(localStorage.getItem("items")) || []);
-  const [style, setStyle] = useState("CorrectInput")  
+
+  const [style, setStyle] = useState("CorrectInput")
+  const [checked, setChecked] = useState(false)
 
 
   useEffect(() => {
     localStorage.setItem("items", JSON.stringify(items))
   }, [items])
 
+
   function SetTheDarkMode() {
     setDarkMode(prevDarkMode => !prevDarkMode)
   }
 
-  function addItem() {
-    if (!newItem) {
+
+
+  function addItem(item) {
+    if (!newItem || !newTitle) {
       setStyle("Incorrectinput");
       return;
     }
-    
-    const item = {
+
+    item = {
       id: Math.floor(Math.random() * 1000),
+      title: newTitle,
       value: newItem,
+      checked: false
     };
 
     setItems((oldList) => [...oldList, item]);
 
     setNewItem("");
+    setnewTitle("");
+    console.log(item)
   }
 
   function deleteItem(id) {
@@ -41,9 +51,14 @@ function App() {
     setItems(newArray);
   }
 
-  function deleteAll () {
+  function deleteAll() {
     setItems([])
-  } 
+  }
+
+  const handleCheckBox = (e) => {
+    console.log(e.target.checked)
+  }
+
 
   return (
     <div className={darkMode ? "App" : "App Dark"}>
@@ -56,40 +71,48 @@ function App() {
         <h1>Add a new item to do</h1>
         <input
           type="text"
-          placeholder="Add an item..."
+          placeholder='Add a title'
+          value={newTitle}
+          onChange={(e) => { setnewTitle(e.target.value); setStyle("CorrectInput") }}
+          className={style}
+        />
+        <input
+          type="text"
+          placeholder="Add an items..."
           value={newItem}
-          onChange={(e) =>{setNewItem(e.target.value); setStyle("CorrectInput")}}
-          className = {style}
+          onChange={(e) => { setNewItem(e.target.value); setStyle("CorrectInput") }}
+          className={style}
         />
         <div className='AddItems--Button-Counter'>
           <button onClick={() => addItem()} className="AddItemsButton">Add</button>
           <button onClick={deleteAll} className='DeleteAllButton'>Delte all</button>
-          <br/>
+          <br />
           <span>You have {items.length} task to do</span>
         </div>
       </div>
 
       <div className='AddedTodo--Container'>
-        <ul>
-          {items.map((x) => {
-            return (
-              <div>
-                <li key={x.id} >
-                  <input type="checkbox"
-                    name='CheckBox'
-                  />
-                  {x.value}
-                  <button
-                    className="delete-button"
-                    onClick={() => deleteItem(x.id)}
-                  >
-                    ❌
-                  </button>
-                </li>
+        {items.map((x) => {
+          return (
+            <div className='TaskDiv' key={x.id} >
+              <div className='Title--Container'>
+                <input type="checkbox"
+                  name='checkbox'
+                  onChange={handleCheckBox}
+                />
+                <h2>{x.title}</h2>
+                <button
+                  className="delete-button"
+                  onClick={() => deleteItem(x.id)}
+                >
+                  ❌
+                </button>
               </div>
-            );
-          })}
-        </ul>
+              <span>{x.value}</span>
+              <div> checked: {x.checked}</div>
+            </div>
+          );
+        })}
       </div>
 
     </div>
